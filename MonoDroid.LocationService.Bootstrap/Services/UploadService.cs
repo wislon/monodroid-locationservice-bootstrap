741ace72888144c5a9852529e09157dc7d1bcb06
John W <wislon@gmail.com>
2013-02-12 20:14:30 +1000

    #11 Created UploadService base class

---
+++ MonoDroid.LocationService.Bootstrap/Services/UploadService.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Content;
+
+namespace MonoDroid.LocationService.Bootstrap.Services
+{
+
+    [Service(Label = "LMS Upload Service")]
+    public class UploadService : IntentService
+    {
+        protected override void OnHandleIntent(Intent intent)
+        {
+            
+        }
+    }
+}
