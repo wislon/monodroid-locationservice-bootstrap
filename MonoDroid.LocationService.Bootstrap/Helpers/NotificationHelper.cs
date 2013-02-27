5aae5b164faffcddcb03a1bd0f8aff8c44988f4e
John W <wislon@gmail.com>
2013-02-27 12:31:50 +1000

    Some minor refactoring

---
+++ MonoDroid.LocationService.Bootstrap/Helpers/NotificationHelper.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Locations;
+using Android.OS;
+using Android.Util;
+using Android.Widget;
+
+namespace MonoDroid.LocationService.Bootstrap.Helpers
+{
+    public class NotificationHelper
+    {
+        public static void SendBroadcastForToastMessage(Context context, string messageToToast)
+        {
+            var toastIntent = new Intent(AppConstants.APPLICATION_COMMAND);
+            toastIntent.PutExtra(AppConstants.COMMAND_TYPE_ID, (int)AppConstants.ApplicationCommandType.ShowToastMessage);
+            toastIntent.PutExtra(AppConstants.TOAST_MESSAGE_KEY, messageToToast);
+            context.SendBroadcast(toastIntent);
+        }
+
+
+    }
+}
