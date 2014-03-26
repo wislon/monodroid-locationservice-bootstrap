f4e63f31dc688574459042d380347cd232e1e853
John Wilson <wislon@users.noreply.github.com>
2014-03-26 21:32:42 +1000

    Update PreferencesHelper.cs
    Now properly parses int, long, double and float values. Funny, this was working before, don't remember ever having to do a TryParse() on any of those...

--- MonoDroid.LocationService.Bootstrap/Helpers/PreferencesHelper.cs
+++ MonoDroid.LocationService.Bootstrap/Helpers/PreferencesHelper.cs
@@ -1,65 +1,81 @@
 using Android.Content;
 using Android.Preferences;
 
-namespace MonoDroid.LocationService.Bootstrap.Helpers
+namespace DashCam.Classes.Helpers
 {
-    public static class PreferencesHelper
+  public static class PreferencesHelper
+  {
+    private static ISharedPreferences _sharedPreferences = null;
+
+    public static string GetPreferenceAsString(Context context, string preferenceKey)
     {
-        private static ISharedPreferences _sharedPreferences = null;
+      if (_sharedPreferences == null)
+      {
+        _sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(context);
+      }
 
-        public static string GetPreferenceAsString(Context context, string preferenceKey)
-        {
-            if (_sharedPreferences == null)
-            {
-                _sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(context);
-            }
-
-            var value = _sharedPreferences.GetString(preferenceKey, string.Empty);
-            return value;
-        }
-
-        public static bool GetPreferenceAsBool(Context context, string preferenceKey)
-        {
-            if (_sharedPreferences == null)
-            {
-                _sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(context);
-            }
-
-            var value = _sharedPreferences.GetBoolean(preferenceKey, false);
-            return value;
-        }
-
-        public static long GetPreferenceAsLong(Context context, string preferenceKey)
-        {
-            if (_sharedPreferences == null)
-            {
-                _sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(context);
-            }
-
-            var value = _sharedPreferences.GetLong(preferenceKey, 0);
-            return value;
-        }
-
-        public static int GetPreferenceAsInt(Context context, string preferenceKey)
-        {
-            if (_sharedPreferences == null)
-            {
-                _sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(context);
-            }
-
-            var value = _sharedPreferences.GetInt(preferenceKey, 0);
-            return value;
-        }
-
-        public static float GetPreferenceAsFloat(Context context, string preferenceKey)
-        {
-            if (_sharedPreferences == null)
-            {
-                _sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(context);
-            }
-
-            var value = _sharedPreferences.GetFloat(preferenceKey, 0);
-            return value;
-        }
+      var value = _sharedPreferences.GetString(preferenceKey, string.Empty);
+      return value;
     }
+
+    public static bool GetPreferenceAsBool(Context context, string preferenceKey)
+    {
+      if (_sharedPreferences == null)
+      {
+        _sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(context);
+      }
+
+      var value = _sharedPreferences.GetString(preferenceKey, "false");
+      bool returnVal = bool.TryParse(value, out returnVal) && returnVal;
+      return returnVal;
+    }
+
+    public static long GetPreferenceAsLong(Context context, string preferenceKey)
+    {
+      if (_sharedPreferences == null)
+      {
+        _sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(context);
+      }
+
+      var value = _sharedPreferences.GetString(preferenceKey, "0");
+      long returnVal = long.TryParse(value, out returnVal) ? returnVal : 0;
+      return returnVal;
+    }
+
+    public static int GetPreferenceAsInt(Context context, string preferenceKey)
+    {
+      if (_sharedPreferences == null)
+      {
+        _sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(context);
+      }
+
+      var value = _sharedPreferences.GetString(preferenceKey, "0");
+      int returnVal = int.TryParse(value, out returnVal) ? returnVal : 0;
+      return returnVal;
+    }
+
+    public static double GetPreferenceAsDouble(Context context, string preferenceKey)
+    {
+      if (_sharedPreferences == null)
+      {
+        _sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(context);
+      }
+
+      var value = _sharedPreferences.GetString(preferenceKey, "0");
+      double returnVal = double.TryParse(value, out returnVal) ? returnVal : 0;
+      return returnVal;
+    }
+
+    public static float GetPreferenceAsFloat(Context context, string preferenceKey)
+    {
+      if (_sharedPreferences == null)
+      {
+        _sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(context);
+      }
+
+      var value = _sharedPreferences.GetString(preferenceKey, "0");
+      float returnVal = float.TryParse(value, out returnVal) ? returnVal : 0;
+      return returnVal;
+    }
+  }
 }
